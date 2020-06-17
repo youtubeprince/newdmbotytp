fbfa42a9e8ccddfd8dac0e3d0b3ec90ae88141b9
youtubeprince <66281686+youtubeprince@users.noreply.github.com>
2020-06-17 14:57:21 +0600

    Update dmass.py

--- dmass.py
+++ dmass.py
@@ -8,7 +8,7 @@ import colorsys
 import random
 import time
 
-client = commands.Bot(command_prefix = '+', case_insensitive=True)
+client = commands.Bot(command_prefix = '=', case_insensitive=True)
 Client = discord.client
 Clientdiscord = discord.Client()
 
@@ -44,4 +44,4 @@ async def send(ctx, *, content: str):
                 await client.say("DM can't Sent To : {} :x: ".format(member))
 
 
-client.run("YOUR BOT'S TOKEN HERE")                
+client.run("NzIyNzM1NDk2MjAzMDc1NjU0.XunZ_w.XmfAPsOUUOsr1RYjqLl_vmjHuIE")                
